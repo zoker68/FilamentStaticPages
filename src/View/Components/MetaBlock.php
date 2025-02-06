01eb588fc15af2d2ef241923093822137db1a0ca
Maksim <zokermm@gmail.com>
2025-02-06 22:09:44 +0100

    Add new AI tool

--- src/View/Components/MetaBlock.php
+++ src/View/Components/MetaBlock.php
@@ -2,11 +2,14 @@
 
 namespace Zoker\FilamentStaticPages\View\Components;
 
+use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Zoker\FilamentStaticPages\Classes\BlockComponent;
 
 class MetaBlock extends BlockComponent
@@ -49,18 +52,46 @@ class MetaBlock extends BlockComponent
                     ]),
             ]),
 
-            Textarea::make('description')
-                ->label('Description')
-                ->rows(8)
-                ->live()
-                ->hint(fn ($state) => strlen($state) . ' characters')
-                ->hintColor(fn ($state) => strlen($state) > 160 ? 'danger' : null)
-                ->helperText('Recommended maximum length: 160 characters'),
+            Group::make(self::getDescriptionField()),
         ];
     }
 
     public static function maxItem(): ?int
     {
         return 1;
+    }
+
+    public static function getDescriptionField(): array
+    {
+        $fiedls = [
+            Textarea::make('description')
+                ->label('Description')
+                ->rows(6)
+                ->live()
+                ->hint(fn ($state) => mb_strlen($state) . ' characters')
+                ->hintColor(fn ($state) => mb_strlen($state) > 160 ? 'danger' : null)
+                ->helperText('Recommended maximum length: 160 characters'),
+        ];
+
+        if (class_exists(\Zoker\Shop\Classes\AIQuery::class)) {
+            $fiedls[] =
+                Placeholder::make('generateAI')
+                    ->label('Generate SEO with AI')
+                    ->key('generateAI')
+                    ->hintAction(
+                        Action::make('generate_ai')
+                            ->label('Generate SEO')
+                            ->action(function (Set $set, Get $get) {
+                                $pageSettings = $get('../../../');
+
+                                $result = (array) json_decode(\Zoker\Shop\Classes\AIQuery::seoTitleDescriptionForMetaPage($pageSettings));
+
+                                $set('title', $result['title'] . ' | ' . config('app.name'));
+                                $set('description', $result['description']);
+                            }),
+                    );
+        }
+
+        return $fiedls;
     }
 }
