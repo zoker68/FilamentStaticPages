336ae30377e26cdd4d133c7eb2fbcb3795160de9
Maksim <zokermm@gmail.com>
2025-02-07 21:27:41 +0100

    Edit slider template

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="container">
     <div class="w-full flex items-center">
         <!-- Slider main container -->
         <div class="swiper banner-swiper w-full">
@@ -6,32 +6,43 @@
             <div class="swiper-wrapper">
                 <!-- Slides -->
                 @foreach($slides as $slide)
-                    <div class="swiper-slide flex items-center bg-no-repeat bg-cover bg-center min-h-[520px]"
-                         style="background-image: url({{ $storageUrl . $slide['image'] }});">
-                        <div class="container">
-                            <div class="lg:flex flex-wrap">
-                                <div class="w-full lg:w-1/2">
-                                    <div>
-                                        @if($slide['heading'] ?? false)
-                                        <h1
-                                            class="text-[38px] md:text-[56px] lg:text-5xl xl:text-[56px] leading-[42px] md:leading-[64px] lg:leading-[48px] xl:leading-[64px] font-medium mb-2 sm:mb-4 text-secondary">
-                                            {{ $slide['heading'] }}
-                                        </h1>
-                                        @endif
-                                        @if($slide['text'] ?? false)
-                                        <p class="text-secondary text-base mb-2 sm:mb-4">
-                                            {!! $slide['text'] !!}
-                                        </p>
-                                        @endif
-                                        @if($slide['button'] ?? false)
-                                        <div class="mt-[30px] md:mt-[40px]">
-                                            <a class="primary-btn py-2.5" href="{{ $slide['link'] }}" target="{{ $slide['target'] }}" tabindex="-1">{{ $slide['button'] }}</a>
-                                        </div>
+                    <div class="swiper-slide">
+                        @if(($slide['link'] ?? false) && !($slide['button'] ?? false))
+                            <a href="{{ $slide['link'] }}" target="{{ $slide['target'] }}">
+                                @endif
+                                <div
+                                    class="flex items-center bg-no-repeat bg-cover bg-center min-h-[130px] md:min-h-[250px] lg:min-h-[330px] xl:min-h-[400px]"
+                                    style="background-image: url({{ $storageUrl . $slide['image'] }});">
+                                    <div class="lg:flex flex-wrap">
+                                        @if(($slide['heading'] ?? false) || ($slide['text'] ?? false) || ($slide['button'] ?? false))
+                                            <div class="w-full lg:w-1/2">
+                                                <div>
+                                                    @if($slide['heading'] ?? false)
+                                                        <h1
+                                                            class="text-[38px] md:text-[56px] lg:text-5xl xl:text-[56px] leading-[42px] md:leading-[64px] lg:leading-[48px] xl:leading-[64px] font-medium mb-2 sm:mb-4 text-secondary">
+                                                            {{ $slide['heading'] }}
+                                                        </h1>
+                                                    @endif
+                                                    @if($slide['text'] ?? false)
+                                                        <p class="text-secondary text-base mb-2 sm:mb-4">
+                                                            {!! $slide['text'] !!}
+                                                        </p>
+                                                    @endif
+                                                    @if($slide['button'] ?? false)
+                                                        <div class="mt-[30px] md:mt-[40px]">
+                                                            <a class="primary-btn py-2.5" href="{{ $slide['link'] }}"
+                                                               target="{{ $slide['target'] }}"
+                                                               tabindex="-1">{{ $slide['button'] }}</a>
+                                                        </div>
+                                                    @endif
+                                                </div>
+                                            </div>
                                         @endif
                                     </div>
                                 </div>
-                            </div>
-                        </div>
+                                @if(($slide['link'] ?? false) && !($slide['button'] ?? false))
+                            </a>
+                        @endif
                     </div>
                 @endforeach
 
